36790436a67a030d95b6ad055166609dfc507fa2
Logan Williams <logan.williams@alum.mit.edu>
2016-06-16 16:04:04 -0700

    Moved tests to test/runtests.jl

--- src/Pyramids.jl
+++ src/Pyramids.jl
@@ -1,6 +1,6 @@
 module Pyramids
 
-using Images, Interpolations, Base.Test
+using Images, Interpolations
 
 export ImagePyramid, PyramidType, ComplexSteerablePyramid, LaplacianPyramid, GaussianPyramid
 export subband, toimage, update_subband, update_subband!, test
@@ -181,46 +181,6 @@ function toimage(pyramid::ImagePyramid)
     end
 
     return im
-end
-
-##############################
-# Tests
-
-function test()      
-    function end_to_end(T, convert_to_arr=true)
-        zone_plate = load("test/zone_plate.png")
-
-        if convert_to_arr
-            zone_plate = convert(Array, zone_plate)
-            zone_plate = convert(Array, separate(zone_plate))
-        end
-
-        if typeof(T) <: ComplexSteerablePyramid
-            scale = 0.5^(1/4);
-            pyramid = ImagePyramid(zone_plate, T, scale=scale, num_orientations=8, max_levels=23, min_size=15, twidth=1.0)
-        else
-            pyramid = ImagePyramid(zone_plate, T, max_levels=23, min_size=15)
-        end
-
-        zone_plate_recon = toimage(pyramid)
-
-        return all(zone_plate_recon .- zone_plate .< 1e-4)
-    end
-
-    println("Running end-to-end image comparison test for Complex Steerable Pyramid.")
-    @test end_to_end(ComplexSteerablePyramid())
-    println("\twithout array conversion")
-    @test end_to_end(ComplexSteerablePyramid(), convert_to_arr=false)
-    
-    println("Running end-to-end image comparison test for Gaussian Pyramid.")
-    @test end_to_end(GaussianPyramid())
-    println("\twithout array conversion")
-    @test end_to_end(GaussianPyramid(), convert_to_arr=false)
-
-    println("Running end-to-end image comparison test for Laplacian Pyramid.")
-    @test end_to_end(LaplacianPyramid())
-
-    println("Tests passing.")
 end
 
 ##############################
